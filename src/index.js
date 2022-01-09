8d518d14b6bee7948baeb85e7c31a8aabbcfa7f2
Cuh4a <82233194+Cuh4a@users.noreply.github.com>
2022-01-09 09:17:58 +0300

    upload index.html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let b = Math.abs(n);
+    let array = ("" + b).split("").map(Number);
+    array.reverse();
+    const singleNumber = Number(array.join(''));
+    return singleNumber;
 }
